edaaa1e893783f4c1223c16e0176494cbc905ca9
Grekus14 <georgepopovich14@gmail.com>
2021-01-10 13:56:09 +0100

    Add leaderboards route

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,7 +1,8 @@
+// Auth token should be prefixed with "Bearer " (e.g. "Bearer mytoken123")
 import { NextFunction, Request, Response } from 'express';
 import { decryptToken } from '../helpers/token';
 
-// Auth token should be prefixed with "Bearer " (e.g. "Bearer mytoken123")
+// Sets req.userId to the ID from the token. Only calls "next()" when token is valid
 export function authUser(req: Request, res: Response, next: NextFunction) {
 	const authHeader = req.headers['authorization'];
 	const token = authHeader?.split(' ')[1];
@@ -15,5 +16,18 @@ export function authUser(req: Request, res: Response, next: NextFunction) {
 	if (!decrypted) return res.status(401).send('Invalid token');
 
 	req.userId = decrypted.id;
+	next();
+}
+
+// Same as authUser, however "next()" regardless if token is present or valid
+export function authUserNotStrict(req: Request, res: Response, next: NextFunction) {
+	const authHeader = req.headers['authorization'];
+	const token = authHeader?.split(' ')[1];
+
+	const decrypted = token ? decryptToken(token) : null;
+	if (decrypted) {
+		req.userId = decrypted.id;
+	}
+
 	next();
 }
--- src/routes/other.ts
+++ src/routes/other.ts
@@ -1,9 +1,8 @@
 import { Router } from 'express';
 import User from '../models/user.model';
 import { signToken } from '../helpers/token';
-import { authUser } from '../middleware/auth';
+import { authUser, authUserNotStrict } from '../middleware/auth';
 const router = Router();
-
 
 router.get('/me', authUser, async (req, res) => {
 	res.json(await User.findById(req.userId).select('-_id'));
@@ -29,8 +28,31 @@ router.get('/now', (req, res) => {
 	res.json({ timestamp: new Date().getTime() });
 });
 
-router.get('/leaderboards', (req, res) => {
-	res.send('Work in progress');
+router.get('/leaderboards', authUserNotStrict, async (req, res) => {
+	const isSignedIn = !!req.userId;
+	console.log(isSignedIn);
+	const [leaders, myPoints] = await Promise.all([
+		User.aggregate([
+			{ $sort: { points: -1 } },
+			{ $limit: 10 },
+			{ $project: { name: true, points: true, _id: false } }
+		]),
+		isSignedIn ? User.findById(req.userId).select('-_id points') : null
+	]);
+
+	for (let i = 0; i < leaders.length; i++) {
+		leaders[i].place = i + 1;
+	}
+
+	// Since the "points" field is indexed this should be fairly efficient
+	const myPlace = isSignedIn
+		? (await User.count({ points: { $gt: myPoints.points } })) + 1
+		: null;
+
+	res.json({
+		leaders,
+		...(isSignedIn ? { current_user_place: myPlace } : {})
+	});
 });
 
 export default router;
