1dbb26322c5dbedc661da06443eabec4b254cb1e
Grekus14 <georgepopovich14@gmail.com>
2021-01-10 11:13:46 +0100

    Add claim_bonus route

--- src/routes/game.ts
+++ src/routes/game.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import { authUser } from '../middleware/auth';
 import User from '../models/user.model';
+import mongoose from 'mongoose';
 const router = Router();
 
 router.post('/play', authUser, async (req, res) => {
@@ -9,10 +10,11 @@ router.post('/play', authUser, async (req, res) => {
 	lastHour.setMinutes(0, 0, 0);
 	const maxGamesPerHour = 5;
 
-	const result = await User.findOneAndUpdate(
+	const user = await User.findOneAndUpdate(
 		{
 			_id: req.userId,
 			$expr: {
+				// This makes sure there are less then 5 games played in the last hour
 				$lt: [
 					{
 						$size: {
@@ -29,21 +31,50 @@ router.post('/play', authUser, async (req, res) => {
 		},
 		{
 			$inc: { points },
-			$push: { recentGameDates: new Date() },
-			$pull: { $lt: lastHour }
+			$push: { recentGameDates: new Date() }
 		}
 	);
 
-	if (!result) return res.status(403).send('Max requests this hour reached');
+	if (!user) return res.status(403).send('Max requests this hour reached');
 
 	res.json({
 		points_added: points,
-		points_total: points + result.points
+		points_total: points + user.points
 	});
+
+	// This gets rid of the old games which are no longer needed
+	User.updateOne(
+		{
+			_id: req.userId
+		},
+		{
+			$pull: { recentGameDates: { $lte: lastHour } }
+		}
+	);
 });
 
 router.post('/claim_bonus', authUser, async (req, res) => {
-	res.sendStatus(200);
+	const oneMinuteAgo = new Date(new Date().getTime() - 60 * 1000);
+
+	const user = await User.findOneAndUpdate(
+		{ _id: req.userId, lastBonusClaim: { $lte: oneMinuteAgo } },
+		{ $set: { lastBonusClaim: new Date() } }
+	);
+
+	if (!user) return res.status(403).send('Please wait at least a minute');
+
+	// (difference in ms / 60000) * 10 === 10 points per minute
+	const points = Math.min(
+		Math.floor((new Date().getTime() - user.lastBonusClaim) / 60000) * 10,
+		100
+	);
+
+	await User.updateOne({ _id: req.userId }, { $inc: { points: points } });
+
+	res.json({
+		points_added: points,
+		points_total: user.points + points
+	});
 });
 
 export default router;
